4be4e369a897e0ab359107c4020c327992b20a96
AlphaServiceMasters <garen.1957.99@gmail.com>
2025-05-19 00:24:34 +0100

    Delete contact.js

--- contact.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('loginBtn').addEventListener('click', function() {
-    alert('Login functionality coming soon!');
-});
-
-document.querySelector('.contact-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    document.getElementById('contactMsg').textContent = 'Thank you for contacting us!';
-    this.reset();
-});
