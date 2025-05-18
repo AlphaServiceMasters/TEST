26249b5d8e4beb2e4a64ade6ac5bd7d193182197
AlphaServiceMasters <garen.1957.99@gmail.com>
2025-05-19 00:24:43 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('loginBtn').addEventListener('click', function(e) {
-    this.classList.add('clicked');
-    setTimeout(() => this.classList.remove('clicked'), 200);
-    alert('Login functionality coming soon!');
-});
